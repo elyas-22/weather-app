8ec822d8026df7b45e3fcdd8ec121f5b96f576c4
elyas-22 <elyaschaudhary@live.com>
2023-05-01 23:14:21 +0100

    Working on forecastsummaries tests

--- src/components/ForecastSummary.js
+++ src/components/ForecastSummary.js
@@ -5,7 +5,7 @@ function ForecastSummary(props) {
   const longdate = new Date(date);
   const newdate = longdate.toDateString(0);
   return (
-    <div className="forecast-summary">
+    <div className="forecast-summary" data-testid="forecast-summary">
       <div className="forecast-summary_date">{newdate}</div>
       <div className="forecast-summary_description">{description}</div>
       <div className="forecast-summary_icon" data-testid="forecast-icon">
